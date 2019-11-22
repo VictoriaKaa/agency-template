0ba67a4e42ece7c51dfd724259f2cb484766f9d9
VictoriaKaa <victoriaklimova96@gmail.com>
2019-11-22 16:43:44 +0300

    Updates

---
+++ precache-manifest.f83ac20da3aaaa0cc5482462f0bc1712.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea484cb6d0c06d707c0ac9c1aece0ee8",
+    "url": "/agency-template/index.html"
+  },
+  {
+    "revision": "9d5fef0b09b15ed6fea6",
+    "url": "/agency-template/static/css/2.2834e29e.chunk.css"
+  },
+  {
+    "revision": "898eb83849f224572ce4",
+    "url": "/agency-template/static/css/main.4b5e8fdd.chunk.css"
+  },
+  {
+    "revision": "9d5fef0b09b15ed6fea6",
+    "url": "/agency-template/static/js/2.c7f70558.chunk.js"
+  },
+  {
+    "revision": "898eb83849f224572ce4",
+    "url": "/agency-template/static/js/main.f9b5d3ea.chunk.js"
+  },
+  {
+    "revision": "fe358c00dd2c5503b78d",
+    "url": "/agency-template/static/js/runtime-main.c7ba3583.js"
+  },
+  {
+    "revision": "cbe53e85c3850d121439b64bab0f690d",
+    "url": "/agency-template/static/media/action.cbe53e85.png"
+  },
+  {
+    "revision": "c756206341821880a4a506b5c3532af0",
+    "url": "/agency-template/static/media/home-background.c7562063.png"
+  },
+  {
+    "revision": "04f1a764e3200985381a26154589a3f2",
+    "url": "/agency-template/static/media/icomoon.04f1a764.ttf"
+  },
+  {
+    "revision": "41e0d5eb6d4e9e24414083efea87cec3",
+    "url": "/agency-template/static/media/icomoon.41e0d5eb.eot"
+  },
+  {
+    "revision": "4c764d491d6d0778865e3b5554d5f5aa",
+    "url": "/agency-template/static/media/icomoon.4c764d49.woff"
+  },
+  {
+    "revision": "b3eb6a6eac1db885212bc3862ed84036",
+    "url": "/agency-template/static/media/icomoon.b3eb6a6e.svg"
+  },
+  {
+    "revision": "d62c01319e26e111620cde925f09e974",
+    "url": "/agency-template/static/media/post1.d62c0131.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/agency-template/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/agency-template/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/agency-template/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/agency-template/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "3d8d59f4782518823ea7c9acb889545f",
+    "url": "/agency-template/static/media/work1.3d8d59f4.png"
+  }
+]);
